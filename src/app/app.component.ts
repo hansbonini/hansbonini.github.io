dc6adae4f66112852c9f28be8596327ba6f1f2e4
Hans Bonini <hans@ysolutions.com.br>
2020-04-18 09:12:07 -0300

    Angular Frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, HostListener, ElementRef, ViewChild } from "@angular/core";
 import {
   trigger,
   transition,
@@ -6,7 +6,7 @@ import {
   state,
   style
 } from "@angular/animations";
-import { slideInUp, slideOutUp } from "ng-animate";
+import { fadeIn, fadeOut, slideInUp, slideOutUp } from "ng-animate";
 
 @Component({
   selector: "app",
@@ -17,14 +17,89 @@ import { slideInUp, slideOutUp } from "ng-animate";
       state('closed', style({'display': 'none'})),
       transition("open => closed", useAnimation(slideOutUp)),
       transition("closed => open", [style({'display': 'block' }), useAnimation(slideInUp)])
-    ]), 
+    ]),
+    trigger("scrollDisplay", [
+        state('hide', style({'display': 'none'})),
+        transition("hide => show",[style({'display': 'block' }), useAnimation(fadeIn)]),
+        transition("show => hide",[style({'display': 'none' }), useAnimation(fadeOut)])
+    ])
   ]
 })
 export class AppComponent {
   name = "Meu Site";
 
   MENU_STATE = false;
+  SCROLL_STATE = false;
 
+  private screenHeight: number = 0;
+  private screenWidth: number = 0;
+  public scrollOffset: number = 0;
+  public scrollerOffset: number = 0;
+  public scrollerHeight: number = 0;
+  public scrollerAmount: number = 0;
+  public lastMouseScrollOffset: number = 0;
+  
+  @ViewChild('page') page: ElementRef;
+  
+  @HostListener("window:resize", ["$event"]) responsiveCanvas(event?) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+  }
+  
+  ngOnInit() {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+  }
+  
+  onActivate(event) {
+    this.SCROLL_STATE = false;
+    this.scrollOffset = 0;
+    this.scrollerOffset = 10;
+    this.scrollerHeight = 0;
+    this.scrollerAmount = 0;
+    setTimeout(t => {
+        console.log(this.page.nativeElement.offsetHeight,this.screenHeight)
+        if (this.page.nativeElement.offsetHeight > this.screenHeight) {
+            this.SCROLL_STATE = true;
+            this.scrollerAmount = (this.page.nativeElement.offsetHeight*0.05);
+            this.scrollerHeight = ((this.screenHeight-100)*0.05)
+            console.log(this.scrollerHeight);
+            return;
+        }
+        this.SCROLL_STATE = false;
+    }, 1000);
+    
+  }
+ 
+  scrollUp() {
+    if (this.scrollOffset > 10) {
+        this.scrollOffset -= this.scrollerAmount;
+        this.scrollerOffset -= this.scrollerHeight;
+    }
+  }
+ 
+  scrollDown() {
+    if (this.scrollOffset < this.page.nativeElement.offsetHeight-this.screenHeight) { 
+        this.scrollOffset += this.scrollerAmount;
+        this.scrollerOffset += this.scrollerHeight;
+    }
+  }
+  
+  isWheelUp (event) {
+    if (event.wheelDelta) {
+        return event.wheelDelta > 0;
+    }
+    return event.deltaY < 0;
+  }
+  
+  mouseScroll(event) {
+     if (!this.isWheelUp(event)) {
+        this.scrollDown()
+        return;
+     }
+     this.scrollUp()
+  }
+  
   toggleMenu(clicked: boolean) {
     this.MENU_STATE = this.MENU_STATE ? false : true;
     console.log(this.MENU_STATE);
--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { fadeIn, fadeOut } from 'ng-animate';
 import { DataService } from "../../services/data.service";
 
 @Component({
   selector: "app-post",
   templateUrl: "./post.component.html",
-  styleUrls: ["./post.component.css"]
+  styleUrls: ["./post.component.css"],
 })
 export class PostComponent implements OnInit {
   public html: string = "";
   public title: string;
   public description: string;
-
+  
   constructor(
     private route: ActivatedRoute,
     private router: Router,
@@ -23,7 +25,12 @@ export class PostComponent implements OnInit {
       this.description = response.description;
       this.html = response.html;
     });
+  }d
+  
+  close() {
+    this.router.navigate(['/']);
   }
-
+  
   ngOnInit() {}
+  
 }
